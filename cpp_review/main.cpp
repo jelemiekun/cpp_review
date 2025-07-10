ca3072e521b3dbe44abf765bd70ce4244abfbd85
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-07-10 08:39:30 +0800

    feat(printNumber)

--- cpp_review/main.cpp
+++ cpp_review/main.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <string>
 
 static int multiply(int& firstNumber, int& secondNumber) {
 	return firstNumber * secondNumber;
 }
 
-static void print(std::string& string) {
-	std::cout << string;
+static void printNumber(int& number) {
+	std::cout << number;
 }
 
 int main() {
-	std::string resultString;
-
 	int firstValue = 10;
 	int secondValue = 5;
 	int* pointerToFirst = &firstValue;
 	int product = multiply(*pointerToFirst, secondValue);
 
-	resultString += std::to_string(*pointerToFirst);
-	resultString += " * ";
-	resultString += std::to_string(secondValue);
-	resultString += " = ";
-	resultString += std::to_string(product);
+	printNumber(*pointerToFirst);
 
-	print(resultString);
+	std::cout << " * ";
+
+	printNumber(secondValue);
+
+	std::cout << " = ";
+
+	printNumber(product);
 
 	return EXIT_SUCCESS;
 }
