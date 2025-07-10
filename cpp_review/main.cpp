2130b03641f55fcfc215271b690f4664e37cbf81
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-07-10 09:30:58 +0800

    feat(class)

--- cpp_review/main.cpp
+++ cpp_review/main.cpp
@@ -8,6 +8,19 @@ static void printNumber(int& number) {
 	std::cout << number;
 }
 
+class Car {
+private:
+	static int carCounter;
+
+public:
+	Car() {
+		std::cout << "Car " << carCounter << " created." << '\n';
+		carCounter++;
+	}
+};
+
+int Car::carCounter = 0;
+
 int main() {
 	int firstValue = 10;
 	int secondValue = 5;
@@ -23,6 +36,23 @@ int main() {
 	std::cout << " = ";
 
 	printNumber(product);
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << i << '\n';
+	}
+
+	int** pointer = &pointerToFirst;
+
+	std::cout << pointer << '\n';
+
+	int a = 5;
+	int& rA = a;
+
+	std::cout << "rA = " << rA << '\n';
+
+	for (int i = 0; i < 10; i++) {
+		Car car;
+	}
 
 	return EXIT_SUCCESS;
 }
